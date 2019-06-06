fc5df0b2972600f60ef1dd0126b301aaa56cdfb3
Tataryntseva Svetlana <sitenkosvetlana@gmail.com>
2019-06-06 11:32:33 +0300

    NASA added

--- NASA_spatial_mission/index.js
+++ NASA_spatial_mission/index.js
@@ -8,17 +8,35 @@ countdown(-154800000)  // return  '-43:00:00'
 countdown(0)           // return  '+00:00:00'
 countdown(61000)       // return  '+00:01:01'
 countdown(360000000)   // return '+100:00:00'
-
 */
+function toTimeFormat(num) {
+    if (String(num).length === 1) {
+        return '0' + num;
+    }
+    return num;
+}
 
 
-function countdown(inputNumber) {
-    
+function countdown(inputValueMilliseconds) {
+    const MILLISECONDS_IN_HOUR = 3600000;
+    const MILLISECONDS_IN_MINUTE = 60000;
+    const MILLISECONDS_IN_SECOND = 1000;
+    let result;
 
-    return ;
+    const quantityHours = Math.floor(inputValueMilliseconds / MILLISECONDS_IN_HOUR);
+    const remainderHours = inputValueMilliseconds % MILLISECONDS_IN_HOUR;
+
+    const quantityMinutes = Math.floor(remainderHours / MILLISECONDS_IN_MINUTE);
+    const remainderMinutes = remainderHours % MILLISECONDS_IN_MINUTE;
+
+    const quantitySeconds = Math.floor(remainderMinutes / MILLISECONDS_IN_SECOND);
+    const hoursInTimeFormat = quantityHours >= 0 ? '+' + toTimeFormat(quantityHours) : toTimeFormat(quantityHours);
+
+
+    result = hoursInTimeFormat + ':' + toTimeFormat(quantityMinutes) + ':' + toTimeFormat(quantitySeconds);
+    console.log(result);
+
+    return result;
 }
 
 countdown(-154800000);
-
-
-
--- NASA_spatial_mission/test.js
+++ NASA_spatial_mission/test.js
@@ -2,19 +2,19 @@ describe("countdown", () => {
 
 
     it("countdown(-154800000) return '-43:00:00'", () => {
-        assert.equal(countdown((-154800000), '-43:00:00'));
+        assert.equal(countdown(-154800000), '-43:00:00');
     });
 
     it("countdown(0) return '+00:00:00'", () => {
-        assert.equal(countdown((0), '+00:00:00'));
+        assert.equal(countdown(0), '+00:00:00');
     });
 
     it("countdown(61000) return '+00:01:01'", () => {
-        assert.equal(countdown((61000), '+00:01:01'));
+        assert.equal(countdown(61000), '+00:01:01');
     });
 
     it("countdown(360000000) return '+100:00:00'", () => {
-        assert.equal(countdown((360000000), '+100:00:00'));
+        assert.equal(countdown(360000000), '+100:00:00');
     });
 
 
